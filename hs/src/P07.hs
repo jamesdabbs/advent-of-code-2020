0552ee07218942a7375094a967960b665d8e343f
James Dabbs <jamesdabbs@gmail.com>
2020-12-07 07:58:50 -0800

    Solve day 7

---
+++ hs/src/P07.hs
@@ -0,0 +1,56 @@
+module P07 where
+
+import Import hiding (takeWhile)
+import Data.Attoparsec.Text (anyChar, manyTill)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import Control.Lens ( (%=), at )
+
+type Rule = (Text, [(Int, Text)])
+
+solve :: Text -> IO ()
+solve input = do
+  rules <- parse input parser
+
+  print $ Set.size $ containers rules "shiny gold" -- 121
+  print $ size rules "shiny gold" -- 3805
+
+containers :: [Rule] -> Text -> Set Text
+containers rules = expand Set.empty . parents
+  where
+    -- bag => [bag] that contain it
+    index = flip execState Map.empty $
+      forM rules $ \(outer, inners) ->
+        forM inners $ \(_, inner) ->
+          at inner %= Just . maybe [outer] (outer :)
+
+    parents bag = Map.findWithDefault [] bag index
+
+    expand seen (a:as)
+      | a `Set.member` seen = expand seen as
+      | otherwise = expand (a `Set.insert` seen) (parents a <> as)
+    expand seen _ = seen
+
+size :: [Rule] -> Text -> Int
+size rules = go
+  where
+    -- bag => [(count, bag)] within it
+    index = Map.fromList rules
+
+    go bag = sum $ map (\(n, inner) -> n * (1 + go inner)) $ Map.findWithDefault [] bag index
+
+parser :: Parser [Rule]
+parser = line `sepBy` "\n"
+  where
+    bag = Text.pack <$> manyTill anyChar " bag"
+
+    line = (,)
+      <$> bag <* "s contain "
+      <*> (nothing <|> (content `sepBy` ", ")) <* "."
+
+    content = (,)
+      <$> decimal <* " "
+      <*> bag <* optional "s"
+
+    nothing = "no other bags" $> []
---
+++ hs/test/P07Spec.hs
@@ -0,0 +1,43 @@
+module P07Spec where
+
+import P07
+import SpecImport
+import qualified Data.Set as Set
+
+ex1 :: Text
+ex1 = [here|light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+|]
+
+ex2 :: Text
+ex2 = [here|shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+|]
+
+spec :: Spec
+spec = do
+  let Right i1 = parseOnly parser ex1
+      Right i2 = parseOnly parser ex2
+
+  describe "part 1" $ do
+    it "ex1" $
+      containers i1 "shiny gold" `shouldBe` Set.fromList ["bright white", "muted yellow", "dark orange", "light red"]
+
+  describe "part 2" $ do
+    it "ex1" $ do
+      size i1 "shiny gold" `shouldBe` 32
+
+    it "ex2" $ do
+      size i2 "shiny gold" `shouldBe` 126
