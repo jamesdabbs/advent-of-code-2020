e9727a428d7dc7f3dfab4775ee80b71fcb60097b
James Dabbs <jamesdabbs@gmail.com>
2020-12-09 06:35:05 -0800

    Refactor to use Data.Sequence
    We do a fair amount of tail-appending here, and should use a data structure that supports it efficiently.

--- hs/test/P09Spec.hs
+++ hs/test/P09Spec.hs
@@ -2,33 +2,35 @@ module P09Spec where
 
 import P09
 import SpecImport
+import qualified Data.Sequence as Seq
 
 spec :: Spec
 spec = parallel $ do
-  let input = [ 35
-              , 20
-              , 15
-              , 25
-              , 47
-              , 40
-              , 62
-              , 55
-              , 65
-              , 95
-              , 102
-              , 117
-              , 150
-              , 182
-              , 127
-              , 219
-              , 299
-              , 277
-              , 309
-              , 576
-              ]
+  let input = Seq.fromList
+        [ 35
+        , 20
+        , 15
+        , 25
+        , 47
+        , 40
+        , 62
+        , 55
+        , 65
+        , 95
+        , 102
+        , 117
+        , 150
+        , 182
+        , 127
+        , 219
+        , 299
+        , 277
+        , 309
+        , 576
+        ]
 
   it "finds the first non-sum of previous" $ do
-    scan 5 input `shouldBe` Just 127
+    scanForNonSum 5 input `shouldBe` Just 127
 
   it "finds the consecutive sum" $ do
-    conSum 127 input `shouldBe` Just [15, 25, 47, 40]
+    consecutiveSum 127 input `shouldBe` Just (Seq.fromList [15, 25, 47, 40])
