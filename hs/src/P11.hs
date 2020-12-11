0e6fdae77b41609354dc060b9513bdfd5fbec47f
James Dabbs <jamesdabbs@gmail.com>
2020-12-11 08:09:16 -0800

    Solve day 11
        time                 2.500 s    (2.237 s .. 2.646 s)
                             0.999 R²   (0.997 R² .. 1.000 R²)
        mean                 2.574 s    (2.503 s .. 2.682 s)
        std dev              111.7 ms   (16.67 ms .. 144.2 ms)
    
        	total alloc = 2,350,547,104 bytes  (excludes profiling overheads)
    
        COST CENTRE     MODULE    SRC                        %time %alloc
    
        toggle.adjacent P11       src/P11.hs:(40,5)-(44,14)   57.0   26.3
        evolve.\        P11       src/P11.hs:30:17-81         29.8   60.8
        evolve          P11       src/P11.hs:(29,1)-(32,7)     4.8    6.0
        toggle          P11       src/P11.hs:(35,1)-(44,14)    3.3    2.5
        ==              P11       src/P11.hs:7:19-20           2.8    0.0
        stabilize       P11       src/P11.hs:(23,1)-(26,49)    1.0    2.3

---
+++ hs/src/P11.hs
@@ -0,0 +1,71 @@
+module P11 where
+
+import Import hiding (Location)
+import qualified Data.Map as Map
+
+data Seat = Empty | Occupied | Floor
+  deriving (Show, Eq)
+
+type Location = (Int, Int)
+type Vector = (Int, Int)
+type Seats = Map Location Seat
+type Adjacencies = Map Location [Location]
+
+solution :: Solution Seats
+solution = solve parser $ \seats -> do
+  part1 $ score $ stabilize (neighborhoods seats) 4 seats -- 2178
+  part2 $ score $ stabilize (linesOfSight seats) 5 seats -- 1978
+
+score :: Seats -> Int
+score = length . filter (== Occupied) . Map.elems
+
+stabilize :: Adjacencies -> Int -> Seats -> Seats
+stabilize adjacencies threshold seats
+  | next == seats = seats
+  | otherwise = stabilize adjacencies threshold next
+  where next = evolve adjacencies threshold seats
+
+evolve :: Adjacencies -> Int -> Seats -> Seats
+evolve adjacencies threshold seats = Map.foldrWithKey
+  (\location -> Map.insert location . toggle adjacencies threshold seats location)
+  Map.empty
+  seats
+
+toggle :: Adjacencies -> Int -> Seats -> Location -> Seat -> Seat
+toggle a threshold seats location s
+  | s == Empty && adjacent == 0 = Occupied
+  | s == Occupied && adjacent >= threshold = Empty
+  | otherwise = s
+  where
+    adjacent =
+      Map.findWithDefault [] location a
+      & map (`Map.lookup` seats)
+      & filter (== Just Occupied)
+      & length
+
+neighborhoods :: Seats -> Adjacencies
+neighborhoods seats = Map.fromList [ (loc, neighbors loc) | loc <- Map.keys seats ]
+  where
+    neighbors (x, y) = [ (x + dx, y + dy) | (dx, dy) <- directions ]
+
+linesOfSight :: Seats -> Adjacencies
+linesOfSight seats = Map.fromList [ (loc, visible loc) | loc <- Map.keys seats ]
+  where
+    visible l = mapMaybe (walk l) directions
+
+    walk (x, y) (dx, dy) = case Map.lookup (x + dx, y + dy) seats of
+      Just Floor -> walk (x + dx, y + dy) (dx, dy)
+      Just _ -> Just (x + dx, y + dy)
+      Nothing -> Nothing
+
+directions :: [Vector]
+directions = [ (x,y) | x <- [-1 .. 1], y <- [-1 .. 1], (x, y) /= (0, 0)]
+
+parser :: Parser Seats
+parser = foldl collect Map.empty <$> grid
+  where
+    collect s (x, y, c) = case c of
+      'L' -> Map.insert (x, y) Empty s
+      '#' -> Map.insert (x, y) Occupied s
+      '.' -> Map.insert (x, y) Floor s
+      _ -> s
---
+++ hs/test/P11Spec.hs
@@ -0,0 +1,26 @@
+module P11Spec where
+
+import P11
+import SpecImport
+
+ex :: Text
+ex = [here|#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##|]
+
+spec :: Spec
+spec = parallel $ do
+  let seats = parseRight parser ex
+
+  it "solves part 1" $
+    score (stabilize (neighborhoods seats) 4 seats) `shouldBe` 37
+
+  it "solves part 2" $
+    score (stabilize (linesOfSight seats) 5 seats) `shouldBe` 26
