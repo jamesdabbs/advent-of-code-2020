54b4e13ae6f976badf1fa4417741df2e5c06f8cd
James Dabbs <jamesdabbs@gmail.com>
2020-12-14 18:26:00 -0800

    Optimize day 14
    ∆ 265.5 ms to 136.9 ms on my machine.
    
        time                 265.5 ms   (228.1 ms .. 320.4 ms)
                             0.984 R²   (0.971 R² .. 1.000 R²)
        mean                 292.4 ms   (272.4 ms .. 328.7 ms)
        std dev              38.48 ms   (6.766 ms .. 53.42 ms)
    
    to
    
        time                 136.9 ms   (120.9 ms .. 155.8 ms)
                             0.979 R²   (0.923 R² .. 1.000 R²)
        mean                 142.1 ms   (137.3 ms .. 151.2 ms)
        std dev              9.179 ms   (4.818 ms .. 13.12 ms)
    
    There's still some work to do here. 50-60% of the time and alloc'ing is in `floats` and it seems like there's room for improvement there.

--- hs/src/P14.hs
+++ hs/src/P14.hs
@@ -1,15 +1,18 @@
 module P14 where
 
+import Data.Attoparsec.Text (anyChar, count)
 import qualified Data.Map as Map
 import Import hiding (mask)
 
 type Input = [MaskGroup]
 
-type MaskGroup = (Mask, [(Int, Int)])
+type MaskGroup = (Mask, [(Int64, Int64)])
 
-type Mask = [(Int, Maybe Bit)]
+type Mask = [(Int, Char)]
 
-type RAM = Map Int Int
+type RAM = Map Int64 Int64
+
+type Strategy = Mask -> RAM -> (Int64, Int64) -> RAM
 
 data Bit = O | I deriving (Show, Eq)
 
@@ -18,29 +21,29 @@ solution = solve parser $ \input -> do
   part1 $ sum $ process v1 input -- 12408060320841
   part2 $ sum $ process v2 input -- 4466434626828
 
-process :: (Mask -> RAM -> (Int, Int) -> RAM) -> Input -> RAM
-process apply = foldl processGroup Map.empty
+process :: Strategy -> Input -> RAM
+process apply = foldl' processGroup Map.empty
   where
-    processGroup acc (mask, sets) = foldl (apply mask) acc sets
+    processGroup acc (mask, sets) = foldl' (apply mask) acc sets
 
-v1 :: Mask -> RAM -> (Int, Int) -> RAM
+v1 :: Strategy
 v1 mask ram (addr, value) = Map.insert addr (masked mask value) ram
 
-masked :: Mask -> Int -> Int
-masked mask n = foldr bitmask n mask
+masked :: Mask -> Int64 -> Int64
+masked mask n = foldl' bitmask n mask
   where
-    bitmask (i, Just O) = flip clearBit i
-    bitmask (i, Just I) = flip setBit i
-    bitmask _ = identity
+    bitmask acc (i, '0') = acc `clearBit` i
+    bitmask acc (i, '1') = acc `setBit` i
+    bitmask acc _ = acc
 
-v2 :: Mask -> RAM -> (Int, Int) -> RAM
-v2 mask ram (addr, value) = foldl (\r i -> Map.insert i value r) ram $ floats mask addr
+v2 :: Strategy
+v2 mask ram (addr, value) = Map.fromList [(key, value) | key <- floats mask addr] <> ram
 
-floats :: Mask -> Int -> [Int]
+floats :: Mask -> Int64 -> [Int64]
 floats mask base = foldM f base mask
   where
-    f n (i, Nothing) = [n `setBit` i, n `clearBit` i]
-    f n (i, Just I) = [n `setBit` i]
+    f n (i, 'X') = [n `setBit` i, n `clearBit` i]
+    f n (i, '1') = [n `setBit` i]
     f n _ = [n]
 
 parser :: Parser Input
@@ -51,14 +54,7 @@ parser = commands `sepBy` "\n"
         <$> ("mask = " *> mask <* "\n")
         <*> set `sepBy` "\n"
 
-    mask =
-      fmap (zip [width - 1, width - 2 .. 0]) $
-        many $
-          choice
-            [ "X" $> Nothing,
-              "0" $> Just O,
-              "1" $> Just I
-            ]
+    mask = zip [width - 1, width - 2 .. 0] <$> count width anyChar
 
     set =
       (,)
